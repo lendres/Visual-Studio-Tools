e77a5aaaef3254cf23e613b276ca9186698b9635
lendres <lendres@fifthrace.com>
2017-10-20 14:28:52 -0500

    Adding Templates

---
+++ ItemTemplates/EditMaterialForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace $rootnamespace$
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class $safeitemname$ : Form
+	{
+		#region Members
+
+		#endregion
+
+		#region Construction
+
+		public $safeitemname$()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Event Handlers
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+	} // End class.
+} // End namespace.
---
+++ ItemTemplates/Enum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+	/// <summary>
+	/// 
+	/// The "Description" attribute can be accessed using Reflection to get a string representing the enumeration type.
+	/// 
+	/// The "Length" enumeration can be used in loops as a convenient way of terminating a loop that does not have to be changed if
+	/// the number of items in the enumeration changes.  The "Length" enumeration must be the last item.
+	/// for (int i = 0; i < (int)EnumType.Length; i++) {...}
+	/// </summary>
+	public enum $safeitemrootname$
+	{
+		/// <summary></summary>
+		[Description("")]
+		,
+
+		/// <summary>The number of types/items in the enumeration.</summary>
+		[Description("Length")]
+		Length
+
+	} // End enum.
+} // End namespace.
